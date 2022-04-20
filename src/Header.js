88ab091d27ce0df7e3113c3a827e34b36c3e9ed0
Vitaliy375 <yacenko375@gmail.com>
2022-04-20 18:09:04 +0300

    save changes in react-01

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <a>Home</a>
+                <a>Go Too</a>
+                <a>Servise</a>
+                <a>Contact</a>
+            </ul>
+        </div>
+    );
+}
+export default Header;
---
+++ src/Technologes.js
@@ -0,0 +1,13 @@
+const Technologes = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>less</li>
+            </ul>
+        </div>
+    );
+}
+export default Technologes;
