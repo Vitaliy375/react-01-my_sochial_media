dd7b244560867101693c09456275868d45145fdf
Vitaliy375 <yacenko375@gmail.com>
2022-05-03 16:53:01 +0300

    2 izmenenie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
@@ -7,10 +8,11 @@ import Profile from './components/Profile';
 
 const App = () => {
   return (
-    <div>
+    <div className='app-wrapper'>
       <Header />
       <Navbar />
       <Profile />
+      <Footer />
     </div>
   );
 }
