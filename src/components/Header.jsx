f592ca955ddb410f7b8f8c865eac4aa9a742cd58
Vitaliy375 <yacenko375@gmail.com>
2022-05-03 18:41:52 +0300

    2 izmenenie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return <header className='header'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd3-Kgm4UxjGPB1pmG2CnRKKmNIHDi2SQh6w&usqp=CAU" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd3-Kgm4UxjGPB1pmG2CnRKKmNIHDi2SQh6w&usqp=CAU" /><p>Комфортные походы на любой вкус!</p>
+
     </header>
 }
 
