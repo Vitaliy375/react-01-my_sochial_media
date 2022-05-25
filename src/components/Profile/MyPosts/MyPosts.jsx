3dcf0f466cca252ef36e57e170a7423a02f76207
Vitaliy375 <yacenko375@gmail.com>
2022-05-25 22:29:43 +0300

    15 urok konec

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import mp from './MyPosts.module.css'
+import Post from './Post/Post'
 
 const MyPosts = () => {
 	return (
 		<div>
 			<textarea></textarea>
 			<button>Add post</button>
-			<div>New post</div>
-			<div className={mp.posts}>
-				<div className={mp.item}>
-					<img src='https://cspromogame.ru//storage/upload_images/avatars/4169.jpg' />
-					Post 1
-				</div>
-				<div className={mp.item}>Post 2</div>
-				<div className={mp.item}>Post 3</div>
-				<div className={mp.item}>Post 4</div>
-			</div>
+
+			<div className={mp.posts}></div>
+			<Post />
+			<Post />
+			<Post />
+			<Post />
 		</div>
 	)
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
-import mp from './MyPosts.module.css'
+import mp from './Post.module.css'
 
-const MyPosts = () => {
+const Post = () => {
 	return (
-		<div>
-			<textarea></textarea>
-			<button>Add post</button>
-			<div>New post</div>
-			<div className={mp.posts}>
-				<div className={mp.item}>
-					<img src='https://cspromogame.ru//storage/upload_images/avatars/4169.jpg' />
-					Post 1
-				</div>
-				<div className={mp.item}>Post 2</div>
-				<div className={mp.item}>Post 3</div>
-				<div className={mp.item}>Post 4</div>
+		<div className={mp.item}>
+			<img src='https://cspromogame.ru//storage/upload_images/avatars/4169.jpg' />
+			Post 1
+			<div>
+				<span>like</span>
+			</div>
+			<div>
+				<span>dislike</span>
 			</div>
 		</div>
 	)
 }
 
-export default MyPosts
+export default Post
