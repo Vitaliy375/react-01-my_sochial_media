3a9379679e759d597eb7ccefd4fabf36ac8c0a08
Vitaliy375 <yacenko375@gmail.com>
2022-05-31 15:51:26 +0300

    20 urok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import s from './Navbar.module.css'
+import React from "react"
+import { NavLink } from "react-router-dom"
+import s from "./Navbar.module.css"
 
 const Navbar = () => {
-	return (
-		<nav className={s.nav}>
-			<div className={`${s.item} ${s.active}`}>
-				<a href='/profile'>Profile</a>
-			</div>
-			<div className={s.item}>
-				<a href='/dialogs'>Masseges</a>
-			</div>
-			<div className={s.item}>
-				<a href='/news'>News</a>
-			</div>
-			<div className={s.item}>
-				<a href='/music'>Music</a>
-			</div>
-			<div className={s.item}>
-				<a href='/setting'>Setting</a>
-			</div>
-		</nav>
-	)
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink
+          to="/profile"
+          className={navData => (navData.isActive ? s.active : s.item)}
+        >
+          Profile
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink
+          to="/dialogs"
+          className={navData => (navData.isActive ? s.active : s.item)}
+        >
+          Masseges
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink
+          to="/news"
+          className={navData => (navData.isActive ? s.active : s.item)}
+        >
+          News
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink
+          to="/music"
+          className={navData => (navData.isActive ? s.active : s.item)}
+        >
+          Music
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink
+          to="/setting"
+          className={navData => (navData.isActive ? s.active : s.item)}
+        >
+          Setting
+        </NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import s from './Profile.module.css'
-import MyPosts from './MyPosts/MyPosts'
+import React from "react"
+import s from "./Profile.module.css"
+import MyPosts from "./MyPosts/MyPosts"
 
 const Profile = () => {
   return (
     <div>
-      <div>
-        <img src='https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' />
+      <div className={s.img}>
+        <img src="https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
       </div>
       <div className={s.profile}>ava + description</div>
       <MyPosts />
